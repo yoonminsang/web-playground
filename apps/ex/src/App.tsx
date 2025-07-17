2a367cd435683c0e7c045bed2fb2cabb7348e71f
yoonminsang <apdlyooapdl@gmail.com>
2025-07-18 00:52:11 +0900

    chore: 테스트를 위한 ui 수정

--- apps/ex/src/App.tsx
+++ apps/ex/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Button />
+      <Button>Click me</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
