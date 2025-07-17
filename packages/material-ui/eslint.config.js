c1dee1e7b01b55d840f6197ce1c7f09785bde625
yoonminsang <apdlyooapdl@gmail.com>
2025-07-18 00:44:42 +0900

    setting: lint에서 tsconfig를 못불러오는 문제 해결

--- packages/material-ui/eslint.config.js
+++ packages/material-ui/eslint.config.js
@@ -7,20 +7,30 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default tseslint.config(
+  [
+    globalIgnores(["dist"]),
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs["recommended-latest"],
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        parserOptions: {
+          tsconfigRootDir: __dirname,
+        },
+      },
     },
-  },
-], storybook.configs["flat/recommended"]);
+  ],
+  storybook.configs["flat/recommended"]
+);
