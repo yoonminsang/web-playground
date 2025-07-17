4ea5ea0137528d93659ae28b7574e3e8811c3209
yoonminsang <apdlyooapdl@gmail.com>
2025-07-18 00:45:03 +0900

    feat: material-ui 프로젝트에서 mui를 설치하고 mui Button을 export

--- packages/material-ui/src/components/Button.tsx
+++ packages/material-ui/src/components/Button.tsx
@@ -1,3 +1,8 @@
-export const Button = () => {
-  return <button>Button</button>;
+import { Button as MaterialButton } from "@mui/material";
+import type { ComponentPropsWithRef } from "react";
+
+type Props = ComponentPropsWithRef<typeof MaterialButton>;
+
+export const Button = (props: Props) => {
+  return <MaterialButton {...props} />;
 };
