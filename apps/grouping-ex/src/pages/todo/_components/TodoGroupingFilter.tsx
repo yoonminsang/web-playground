7e518282622c8bda1c20e0671393fa469e47cf48
yoonminsang <apdlyooapdl@gmail.com>
2025-08-04 17:08:01 +0900

    feat: filter에 설명 추가

--- apps/grouping-ex/src/pages/todo/_components/TodoGroupingFilter.tsx
+++ apps/grouping-ex/src/pages/todo/_components/TodoGroupingFilter.tsx
@@ -7,9 +7,12 @@ export interface Props {
 
 export const TodoGroupingFilter = ({ value, onChange }: Props) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value as TodoGroupingOption)}>
-      <option value={TodoGroupingOption.createdAt}>날짜</option>
-      <option value={TodoGroupingOption.category}>카테고리</option>
-    </select>
+    <div>
+      <div>그룹핑</div>
+      <select value={value} onChange={(e) => onChange(e.target.value as TodoGroupingOption)}>
+        <option value={TodoGroupingOption.createdAt}>날짜</option>
+        <option value={TodoGroupingOption.category}>카테고리</option>
+      </select>
+    </div>
   );
 };
--- apps/grouping-ex/src/pages/todo/_components/TodoSortFilter.tsx
+++ apps/grouping-ex/src/pages/todo/_components/TodoSortFilter.tsx
@@ -7,9 +7,12 @@ export interface Props {
 
 export const TodoSortFilter = ({ value, onChange }: Props) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value as TodoSortOption)}>
-      <option value={TodoSortOption.asc}>오름차순</option>
-      <option value={TodoSortOption.desc}>내림차순</option>
-    </select>
+    <div>
+      <div>정렬</div>
+      <select value={value} onChange={(e) => onChange(e.target.value as TodoSortOption)}>
+        <option value={TodoSortOption.asc}>오름차순</option>
+        <option value={TodoSortOption.desc}>내림차순</option>
+      </select>
+    </div>
   );
 };
